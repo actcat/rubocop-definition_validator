87c585c668df03ab3b3e0b3da4604cb85f9a8887
Masataka Kuwabara <p.ck.t22@gmail.com>
2016-05-04 12:10:03 +0900

    Initialize

---
+++ lib/rubocop/diff.rb
@@ -0,0 +1,7 @@
+require "rubocop/diff/version"
+
+module Rubocop
+  module Diff
+    # Your code goes here...
+  end
+end
---
+++ lib/rubocop/diff/version.rb
@@ -0,0 +1,5 @@
+module Rubocop
+  module Diff
+    VERSION = "0.1.0"
+  end
+end
